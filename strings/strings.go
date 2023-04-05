150994f15c29bf7e544f02e956dec0acbd6ef659
Aravind <maravind2000@gmail.com>
2023-04-05 11:42:32 +0530

    Add comment

--- strings/strings.go
+++ strings/strings.go
@@ -1,5 +1,11 @@
 package strings
 
+/**
+	Checks if two strings are perfect anagrams of each other using character count comparison.
+	Anagrams are formed by rearranging the letters of one string using all the original letters exactly once.
+
+	Example: "listen" and "silent" are perfect anagrams of each other, while "now silent" and "listennow" are not.
+**/
 func AreAnagrams(s1, s2 string) bool {
 	// If both strings are of unequal length, they are not anagrams
 	if len(s1) != len(s2) {
