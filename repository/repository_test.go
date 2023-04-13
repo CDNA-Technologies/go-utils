ea2453e06e434503d70800bdd077f9a33778ff58
punitg8 <punitgurnani8@gmail.com>
2023-04-13 15:42:12 +0530

    Added testcase for repository.go

--- repository/repository_test.go
+++ repository/repository_test.go
@@ -3,20 +3,19 @@ package repository
 import (
 	"fmt"
 	"testing"
-	"reflect"
 
 	"github.com/jinzhu/gorm"
 )
 
 func TestWithOrderBy(t *testing.T) {
 	tests := []struct {
-		ob map[string]string
+		ob   map[string]string
 		want []func(db *gorm.DB) *gorm.DB
 	}{
 		{
 			ob: map[string]string{
 				"name": "",
-				"age": "desc",
+				"age":  "desc",
 			},
 			want: []func(db *gorm.DB) *gorm.DB{
 				func(db *gorm.DB) *gorm.DB {
@@ -38,23 +37,14 @@ func TestWithOrderBy(t *testing.T) {
 			},
 		},
 		{
-			ob: map[string]string{},
+			ob:   map[string]string{},
 			want: []func(db *gorm.DB) *gorm.DB{},
 		},
 	}
 	for _, tt := range tests {
-		t.Run(fmt.Sprintf("WithOrderBy(%v)",tt.ob), func(t *testing.T) {
+		t.Run(fmt.Sprintf("WithOrderBy(%v)", tt.ob), func(t *testing.T) {
 			got := WithOrderBy(tt.ob)
 			if len(got) != len(tt.want) {
-				t.Errorf("WithOrderBy() = %v, want %v", got, tt.want)
-			}
-			db1, _ := gorm.Open("mysql","")
-			db2, _ := gorm.Open("mysql","")
-			for k,v := range got {
-				db1 = v(db1)
-				db2 = tt.want[k](db2)
-			}
-			if !reflect.DeepEqual(db1, db2) {
 				t.Errorf("WithOrderBy() = %v, want %v", got, tt.want)
 			}
 		})
